d4cf187d9d968e9f26492c765507497a9da56bb6
mojo718 <153574491+mojo718@users.noreply.github.com>
2024-05-28 20:59:45 -0400

    Create Pets and Owner Model\Schema

---
+++ server/Models/owner.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ownerSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  JoinedOn: {
+    type: Date,
+    default: Date.now,
+  },
+  pets: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Pet',
+    },
+  ],
+});
+
+const Owner = mongoose.model('Owner', ownerSchema);
+
+module.exports = Owner;
