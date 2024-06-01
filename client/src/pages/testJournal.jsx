78df1cbcbb16bb312487138a9a6c2220ffa11de8
Ryan M <ryan.a.moises@gmail.com>
2024-06-01 19:31:37 -0400

    Added auth so that only the owner can view a pet's health information

--- client/src/pages/testJournal.jsx
+++ client/src/pages/testJournal.jsx
@@ -1,20 +1,44 @@
 import TestInfo from '../components/testInfo';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_PET_HEALTH } from '../utils/queries';
+import Auth from '../utils/auth'
 
 const TestJournal = () => {
+  const navigate = useNavigate();
   const { petId } = useParams();
   const { loading, data } = useQuery(QUERY_PET_HEALTH, {
     variables: { petId: petId }
   })
 
+  const handleToHomeClick = () => {
+    navigate('/');
+  }
+
   if (loading) {
     return <div>Trying to fetch pet data...</div>;
   }
 
-
-  // Add Auth here
+  // Auth that only allows owner to view pet health info
+  // Checks for 'id_token' then decodes it if it exists 
+  if (Auth.getToken()) {
+    const userId = Auth.getProfile().data._id
+    if (userId !== data.pet.owner._id ) {
+      return (
+        <>
+          <div>Only the pet's owner can view this! Please return to the homepage!</div>
+          <button onClick={handleToHomeClick}>Return Home</button>
+        </>
+      )
+    }
+  } else if (!Auth.getToken()) {
+    return (
+      <>
+        <div>You must be signed in as the pet's owner to view this page!</div>
+        <button onClick={handleToHomeClick}>Return Home</button>
+      </>
+    )
+  }
 
   return (
     <div>
