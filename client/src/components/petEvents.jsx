8b383638f1c35694978c0ab1103385bacd8bccdc
Ryan M <ryan.a.moises@gmail.com>
2024-06-05 17:46:57 -0400

    addEvent and removeEvent working properly, but need to work on how to save and display time correctly

--- client/src/components/petEvents.jsx
+++ client/src/components/petEvents.jsx
@@ -2,6 +2,9 @@ import { Button, Icon, Modal, ModalContent, ModalActions, Header } from 'semanti
 import './petEvents.css';
 import { useState } from 'react';
 import { useMutation } from '@apollo/client';
+import { QUERY_ME } from '../utils/queries';
+import { ADD_EVENT, REMOVE_EVENT } from '../utils/mutations'
+
 
 // TODO: Get Date to show up as readable format
 
@@ -14,6 +17,8 @@ function PetEvents({ pet }) {
     startTime: ''
   })
 
+  const [addEvent] = useMutation(ADD_EVENT, {refetchQueries: [QUERY_ME]})
+  const [removeEvent] = useMutation(REMOVE_EVENT, {refetchQueries: [QUERY_ME]})
 
   const handleChange = (event) => {
     const { name, value } = event.target
@@ -24,18 +29,54 @@ function PetEvents({ pet }) {
     })
   }
 
+  const handleFormClose = (event) => {
+    toggleAddEvent(false)
+    setFormState({
+      title: '',
+      type: '',
+      location: '',
+      startTime: ''
+    })
+  }
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await addEvent({
+        variables: { ...formState, petId: pet._id}
+      })
+      toggleAddEvent(false)
+    } catch (err) {
+      console.error("Error: Cannot submit event", err)
+    }
+  }
+
+  const handleRemoveEvent = async (event) => {
+    console.log(event.target.dataset.id)
+    try {
+      await removeEvent({
+        variables: {eventId: event.target.dataset.id, petId: pet._id}
+      })
+    } catch (err) {
+      console.error("Error: Cannot remove event", err)
+    }
+  }
+
   return (
     <>
       <div className="events-section">
         <h2>Events for {pet.name}</h2>
-        <button class="ui icon button" onClick={()=>toggleAddEvent(true)}><i aria-hidden="true" class="plus icon"></i></button>
+        <button className="ui icon button" onClick={()=>toggleAddEvent(true)}><i aria-hidden="true" className="plus icon"></i></button>
         {pet.events.length > 0 ? (
           <ul>
             {pet.events.map((event) => (
               <li key={event._id} className="event-item">
                 <h3>{event.title}</h3>
-                <p>{event.description}</p>
+                {event.type ? (<p>{event.type}</p>) : null}
+                <p>Location: {event.location}</p>
                 <p><strong>Date:</strong> {new Date(event.startTime).toLocaleDateString()}</p>
+                <button className="ui icon red button" data-id={event._id} onClick={handleRemoveEvent}><i aria-hidden="true" className="close icon" data-id={event._id}></i></button>
               </li>
             ))}
           </ul>
@@ -50,25 +91,24 @@ function PetEvents({ pet }) {
         onOpen={() => toggleAddEvent(true)}
         open={addEventState}
         size='small'
-        trigger={<Button>Basic Modal</Button>}
       >
         <Header icon>
-          <Icon name='archive' />
+          <Icon name='calendar plus outline' />
           Adding an Event for {pet.name}
         </Header>
         <ModalContent>
           <form>
-            Title: <input></input>
-            Type: <input></input>
-            location: <input></input>
-            startTime: <input></input>   
+            <input type="text" placeholder="Title" name="title" value={formState.title} onChange={handleChange}></input>
+            <input type="text" placeholder="Type" name="type" value={formState.type} onChange={handleChange}></input>
+            <input type="text" placeholder="Location" name="location" value={formState.location} onChange={handleChange}></input>
+            <input type="text" placeholder="Time" name="startTime" value={formState.startTime} onChange={handleChange}></input>   
           </form>
         </ModalContent>
         <ModalActions>
-          <Button basic color='red' inverted onClick={() => toggleAddEvent(false)}>
+          <Button basic color='red' inverted onClick={handleFormClose}>
             <Icon name='remove' /> No
           </Button>
-          <Button color='green' inverted onClick={() => toggleAddEvent(false)}>
+          <Button color='green' inverted onClick={handleFormSubmit}>
             <Icon name='checkmark' /> Yes
           </Button>
         </ModalActions>
